5727c988ef53de90b9dfbcd54523b081df3be5b4
Pablo <pablosgrad12@gmail.com>
2024-05-23 00:09:02 +0900

    Adds code so btn switches color when pressed

--- src/components/RatingNums.jsx
+++ src/components/RatingNums.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 export default function RatingNums({ updateForm }) {
   const ratingNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  function handleRatingSelection(num) {
+  function handleRatingSelection(num, e) {
+    //Make the button clicked grey to show which one is selected and reset the others
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(btn => btn.style.backgroundColor = 'white');
+    e.target.style.backgroundColor = 'grey';
     updateForm({ rating: num });
   };
 
@@ -11,7 +15,7 @@ export default function RatingNums({ updateForm }) {
     <div>
       {ratingNums.map((num, index) => (
         //type is button so it doesn't submit the form autmatically
-        <button key={index} type="button" onClick={() => handleRatingSelection(num)}>{num}</button>
+        <button key={index} type="button" onClick={(e) => handleRatingSelection(num, e)}>{num}</button>
       ))}
     </div>
   )
