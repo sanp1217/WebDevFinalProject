8e8842c462c79c8604b32c5c448c652561393ab5
sanp1217 <pablosgrad12@gmail.com>
2024-07-03 14:36:47 -0600

    Adds games list route

---
+++ src/server/routes/gamesList.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { getDb } from "../db/conn.js";
+
+const gamesListRoutes = express.Router();
+
+//Returns games in users list
+gamesListRoutes.route("/list").get(async function (req, res) {
+	let dbConnect = getDb();
+	const result = await dbConnect.collection("gamesList").find({}).toArray();
+	res.json(result);
+});
+
+//Inserts game to users list
+gamesListRoutes.route("/list/add").post(async function (req, response) {
+	let dbConnect = getDb();
+	let gameToAdd = {
+		name: req.body.name,
+		releaseDate: req.body.releaseDate,
+		image: req.body.image,
+	};
+	const res = await dbConnect.collection("gamesList").insertOne(gameToAdd);
+	response.json(res);
+});
+
+export { gamesListRoutes };
